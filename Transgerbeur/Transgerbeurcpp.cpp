d948667fa90ef6e8a32e68c2c31d9fee8c31ed39
Ilan Manzari <ilanmanzari2607@gmail.com>
2024-11-25 15:23:58 +0100

    commit #2 : Déclarations des variables et définitions des structures, énumérations et unions

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
 
+struct Spalette
+{
+	int poids;
+	char reference[20];
+};
+
+struct Srouleau
+{
+	float longueur;
+	int numero;
+};
+enum Eetat { vide, palette, rouleau };
+
+union Ucont
+{
+	Spalette paletteC;
+	Srouleau rouleauC;
+};
+
+struct SCasier
+{
+	Eetat etat;
+	int px, py;
+	Ucont contenu;
+}; 
+
 void main()
 {
 	cout << "PROGRAMME TRANSGERBEUR";
+	SCasier tabCasier[10];
 }
